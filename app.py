18d41b45cf54f5e528198221bee144e4ceec643d
unknown <patelpunit448@gmail.com>
2026-02-14 12:41:26 +0530

    Changes to be committed: 	modified:   app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("Hello, World!")
 print("This is a simple Python application.")
 print("It demonstrates how to run a Python script.")
+print("You can add more functionality to this application as needed.")
